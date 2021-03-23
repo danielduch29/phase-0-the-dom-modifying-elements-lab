f6bd3bd7b1d7ae0ca58990b3279912f7a9631dcd
Daniel <Daniel.duchatellier@gmail.com>
2021-03-23 19:18:32 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+//const main = document.body.appendChild('main');
+//main.removeChild();
+const mainDel = document.querySelector('main');
+mainDel.remove();
+
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.innerHTML("Daniel is the champion");
+
